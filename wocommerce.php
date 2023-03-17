bed31380953d1f9ec098f34a6b5d96e86f22c889
owaiskhan43 <126915739+owaiskhan43@users.noreply.github.com>
2023-03-17 06:12:33 +0500

    Add files via upload

---
+++ wocommerce.php
@@ -0,0 +1,15 @@
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-3">
+            <div class="cat-col">
+                <?php get_sidebar(); ?>
+            </div>
+        </div>
+        <div class="col-md-9">
+            <div class="wo-shop">
+                  <?php woocommerce_content(); ?>
+            </div>
+        </div>
+    </div>
+  </div>
